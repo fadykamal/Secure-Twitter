6c1ae6cb444df0d3d250593530839a63f2aafef4
Fady Kamal <fady.eldesokey@gmail.com>
2014-05-10 21:19:22 +0200

    added Messaging system base

--- ribbit_app/models.py
+++ ribbit_app/models.py
@@ -4,19 +4,29 @@ import hashlib
 
 
 class Ribbit(models.Model):
-    content = models.CharField(max_length=140)
-    user = models.ForeignKey(User)
-    creation_date = models.DateTimeField(auto_now=True, blank=True)
+	content = models.CharField(max_length=140)
+	user = models.ForeignKey(User)
+	creation_date = models.DateTimeField(auto_now=True, blank=True)
 
 class UserRibbitEncryption(models.Model):
 	user = models.OneToOneField(User)
 	private_key = models.CharField(max_length = 100)
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+	user = models.OneToOneField(User)
+	follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+	
+	def __unicode__(self):
+		return u'%s' % (self.user)
+	def gravatar_url(self):
+		return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
 
-    def gravatar_url(self):
-        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+class Messages(models.Model):
+	sender = models.ForeignKey(User, related_name='sender')
+	receiver = models.ForeignKey(User, related_name='receiver')
+	content = models.CharField(max_length=2048)
+
+	def __unicode__(self):
+		return u'%s %s %s' % (self.sender,":",self.content)
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
